5f4cd4417777cf649a24585dad2bf61d22ff3ce3
charanr27 <30413023+charanr27@users.noreply.github.com>
2017-08-19 19:13:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,30 +1,36 @@
-<html>
-    <head>
-        <title>
-            charan article-one
-        </title>
-    </head>
-    <body>
-        <div>
-            <a href="/">Home</a>
-        </div>
-        <hr/>
-        <h3>
-            charan article-one
-        </h3>
-        <div>
-            aug 19,2017
-        </div>
-        <div>
-            <p>
-                This is the article for my first article
-            </p>
-            <p>
-                Am not gud at coding, from here after i ll learn to code
-            </p>
-            <p>
-                I thought nptel cousrses are easy, but it's tougher than what i thought
-            </p>
-        </div>
-    </body>
-</html>
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+app.use(morgan('combined'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/article-one', function (req, res){
+    res.sendFile(path.join(__dirname, 'ui', 'charan article-one.html'));
+   });
+app.get('/article-two', function (req, res){
+    res.send('Article two requested and will be served here');
+});
+app.get('/article-three', function (req, res){ 
+     res.send('Article three requested and will be served here');
+});
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
