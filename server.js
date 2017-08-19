15e004c9d8a6c3db4375f740ecfffbbc3d20d987
charanr27 <30413023+charanr27@users.noreply.github.com>
2017-08-19 19:38:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/charan article-one', function (req, res){
+app.get('/article-one', function (req, res){
     res.sendFile(path.join(__dirname, 'ui', 'charan article-one.html'));
    });
 app.get('/article-two', function (req, res){
