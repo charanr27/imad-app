a557d3f05aae5f8df419f00eb282fce1b1d1efe6
charanr27 <30413023+charanr27@users.noreply.github.com>
2017-09-02 17:06:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,8 +128,20 @@ app.get('/submit-name', function(req, res) {
     res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function (req, res){
+app.get('/articles/:articleName', function (req, res){
 var articleName = req.params.articleName;
+pool.query("SELECT * FROM article WHERE tilte = " + req.params.articleName, function (err, result) {
+    if (err) {
+        res.status(500).send(err.toString());
+    }else {
+        if (reuslt.rows.length ===0) {
+            res.status(404).send('Article not found');
+        } else {
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+        }
+    }
+});
     res.send(createTemplate(article[articleName]));
    });
 
