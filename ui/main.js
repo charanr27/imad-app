dcbd46f835bd913592beb3a36ff61b73fe5e5968
charanr27 <30413023+charanr27@users.noreply.github.com>
2017-09-05 20:10:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,19 +10,19 @@ submit.onclick = function () {
         
         if (request.readyState === XMLHttpRequest.DONE) {
         if (request.status === 200) {
- var names = request.responseText;
- names = JSON.parse(names);
- var list = '';
- for (var i=0; i< names.length; i++) {
-     list += '<li>' + names[i] + '</li>';
- }
- var ul = document.getElementById('namelist');
- ul.innerHTML = list;
-}
-}
+            alret("Logged in successfully");
+        } else if (request.status === 403) {
+             alert('Username/password is incorrect');
+        } else if (request.status === 500) {
+            alret('something went wrong on the server');
+        }
+        }
 };
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
- request.open('GET', 'http://charansinghr27.imad.hasura-app.io/submit-name?name=' + name, true);
- request.send(null);
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+console.log(username);
+console.log(password);
+ request.open('POST', 'http://charansinghr27.imad.hasura-app.io/submit-name?name=' + name, true);
+ request.setRequestHeader('Content-Type', 'application/json');
+ request.send(JSON.stringfy({username: username, password: password}));
 };
