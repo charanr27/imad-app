dd18d3715ac3b0f8231292d1407ebf2c3a22e4cc
charanr27 <30413023+charanr27@users.noreply.github.com>
2017-09-05 19:54:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ app.post('/login', function (req, res) {
     var username = req.body.username;
     var password = rep.body.password;
     
-    pool.query('SELECT = from "user" username = $1', [username], function (err, result) {
+    pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
     if (err) { 
         res.status(500).send(err.toString());
     } else { 
